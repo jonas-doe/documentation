8e28090b5335e200f1d5563ca4210fe7e60198be
Markus Hunsalz <dev@hunsalz.de>
2021-10-25 13:08:54 +0200

    show confetti

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -2,39 +2,61 @@
   <div class="container bg-ui-background mt-4 lg:mt-8">
     <div class="two-column-layout">
       <div class="relative bg-img-dots">
-        <h2 class="mb-0">{{ $t('home-hero.deploy-smarter') }}</h2>
-        <h2 class="mb-0">{{ $t('home-hero.deploy-faster') }}</h2>
+        <h2 class="mb-0">{{ $t("home-hero.deploy-smarter") }}</h2>
+        <h2 class="mb-0">{{ $t("home-hero.deploy-faster") }}</h2>
         <h2>
           <DeployNow class="text-ui-primary" />
         </h2>
-        <p class="my-8 md:text-xl md:my-16">{{ $t('home-hero.description') }}</p>
+        <p class="my-8 md:text-xl md:my-16">
+          {{ $t("home-hero.description") }}
+        </p>
       </div>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 m-auto <md:max-w-[400px]">
-        <g-link to="/docs/" class="card invert" style="background-color: #11C7E6">
-          <div class="title py-4 lg:pt-10">{{ $t('home-hero.getting-started.title') }}</div>
-          <div>{{ $t('home-hero.getting-started.text') }}</div>
+      <div
+        class="grid grid-cols-1 md:grid-cols-3 gap-4 m-auto <md:max-w-[400px]"
+      >
+        <g-link
+          to="/docs/"
+          class="card invert"
+          style="background-color: #11c7e6"
+        >
+          <div class="title py-4 lg:pt-10">
+            {{ $t("home-hero.getting-started.title") }}
+          </div>
+          <div>{{ $t("home-hero.getting-started.text") }}</div>
         </g-link>
         <g-link to="/docs/framework-samples/" class="card invert">
-          <div class="title py-4 lg:pt-10">{{ $t('home-hero.deploy-sample.title') }}</div>
-          <div>{{ $t('home-hero.deploy-sample.text') }}</div>
+          <div class="title py-4 lg:pt-10">
+            {{ $t("home-hero.deploy-sample.title") }}
+          </div>
+          <div>{{ $t("home-hero.deploy-sample.text") }}</div>
         </g-link>
-        <g-link to="/docs/faq/" class="card invert" style="background-color: #003D8F">
-          <div class="title py-4 lg:pt-10">{{ $t('home-hero.faq.title') }}</div>
-          <div>{{ $t('home-hero.faq.text') }}</div>
+        <g-link
+          to="/docs/faq/"
+          class="card invert"
+          style="background-color: #003d8f"
+        >
+          <div class="title py-4 lg:pt-10">{{ $t("home-hero.faq.title") }}</div>
+          <div>{{ $t("home-hero.faq.text") }}</div>
         </g-link>
       </div>
+    </div>
+
+    <div class="flex-center space-x-2">
+      <button class="btn" @click="start">Start</button>
+      <button class="btn" @click="stop">Stop</button>
+      <button class="btn" @click="love">Show some love</button>
     </div>
 
     <div class="flex justify-center items-center pb-8 pt-12 md:pt-16">
       <a
         href="https://www.producthunt.com/posts/deploy-now?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-deploy-now"
         target="_blank"
-        rel=”noreferrer”
+        rel="”noreferrer”"
       >
         <img
           src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=299768&theme=light"
           alt="Deploy Now - Git push your web project and go live instantly | Product Hunt"
-          style="width: 250px; height: 54px;"
+          style="width: 250px; height: 54px"
           width="250"
           height="54"
         />
@@ -44,11 +66,31 @@
 </template>
 
 <script>
-import DeployNow from '~/components/DeployNow';
+import DeployNow from "~/components/DeployNow";
 
 export default {
   components: {
     DeployNow,
+  },
+  methods: {
+    start() {
+      this.$confetti.start();
+    },
+
+    stop() {
+      this.$confetti.stop();
+    },
+
+    love() {
+      this.$confetti.update({
+        particles: [
+          {
+            type: "heart",
+          },
+        ],
+        defaultColors: ["red", "pink", "#ba0000"],
+      });
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
-import Vuex from 'vuex';
-import createStore from '~/store';
-import DefaultLayout from '~/layouts/Default.vue';
-import 'prism-themes/themes/prism-material-oceanic.css';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
-import 'gridsome-plugin-remark-container/themes/classic.css';
-import '@fontsource/open-sans/latin-400.css';
-import '@fontsource/open-sans/latin-600.css';
-import '@fontsource/open-sans/latin-800.css';
-import '~/assets/styles/app.scss';
+import Vuex from "vuex";
+import VueConfetti from "vue-confetti";
+import createStore from "~/store";
+import DefaultLayout from "~/layouts/Default.vue";
+import "prism-themes/themes/prism-material-oceanic.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "gridsome-plugin-remark-container/themes/classic.css";
+import "@fontsource/open-sans/latin-400.css";
+import "@fontsource/open-sans/latin-600.css";
+import "@fontsource/open-sans/latin-800.css";
+import "~/assets/styles/app.scss";
 
 export default function (Vue, { router, head, appOptions }) {
   Vue.use(Vuex);
   appOptions.store = createStore();
 
+  Vue.use(VueConfetti);
+
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout);
+  Vue.component("Layout", DefaultLayout);
 
   // Add meta data to global head
   //head.link.push({
@@ -22,19 +25,19 @@ export default function (Vue, { router, head, appOptions }) {
   //  href: 'https://fonts.gstatic.com',
   //});
   head.link.push({
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Overpass:wght@100;300&display=swap',
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Overpass:wght@100;300&display=swap",
   });
   head.meta.push({
-    name: 'google-site-verification',
-    content: 'UtLaJ4v0UZBDPJVc8DqCyiUPSSz9TtMroKmflhpH2eo'
-  })
+    name: "google-site-verification",
+    content: "UtLaJ4v0UZBDPJVc8DqCyiUPSSz9TtMroKmflhpH2eo",
+  });
 
   // Add meta data to each page
   router.beforeEach((to, _from, next) => {
     head.meta.push({
-      key: 'og:url',
-      name: 'og:url',
+      key: "og:url",
+      name: "og:url",
       content: process.env.GRIDSOME_BASE_PATH + to.path,
     });
     next();
